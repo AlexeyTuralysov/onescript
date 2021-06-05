b132b5b75163f86f8883cef2309c5be7711ee78c
Alexey Turalysov <72796090+AlexeyMarkul@users.noreply.github.com>
2021-06-05 09:52:19 +0900

    Add files via upload

---
+++ accountsonescript.py
@@ -0,0 +1,72 @@
+
+
+
+import sqlite3
+import time
+global sql
+
+
+
+
+db = sqlite3.connect('Account.db')
+sql = db.cursor()
+
+
+
+sql.execute("""CREATE TABLE IF NOT EXISTS Account (
+    ID INTEGER PRIMARY KEY,
+    NAME TEXT,
+    PASS TEXT
+    
+)""")
+
+
+    
+    
+ 
+db.commit()   
+ 
+
+
+
+
+
+
+def accountonescript():
+      
+  
+  x = 1
+  
+  while(True):
+    
+    
+    if x <= 4: # кол аккаунтов
+      
+      
+      
+      sql.execute(f"SELECT NAME FROM Account WHERE ID = '{x}'") #имя аккаунта по id
+      namelog = str(sql.fetchone()[0])
+      print(namelog)# имя акканта x
+  
+      sql.execute(f"SELECT PASS FROM Account WHERE ID = '{x}'") #пароль аккаунта по id
+  
+      passlog = str(sql.fetchone()[0])
+      print(passlog) # пароль аккаунта x
+      
+      print('аккаунт' x)
+      x += 1
+      
+
+    else:
+      
+    
+      print('Цикл окончен, i =', x)
+      time.sleep(4)
+      accountonescript()
+        
+        
+        
+      
+  
+
+accountonescript()
